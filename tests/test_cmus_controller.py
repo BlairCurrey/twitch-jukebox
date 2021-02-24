54edf0664765397e5ef1eb61fad6ef10688e9973
Blair Currey <curreyb88@gmail.com>
2021-02-24 15:32:58 -0500

    updated tests

--- tests/test_cmus_controller.py
+++ tests/test_cmus_controller.py
@@ -5,48 +5,44 @@ import unittest
 # Local imports
 from src.cmus_controller import CmusController
 
+"""
+Assumes cmus is playing before starting tests
+"""
 
 class CmusControllerTestSet(unittest.TestCase):
-    
-    """
-    Assumes cmus is playing before starting tests
-    """
-
     def __init__(self, *args, **kwargs):
         super(CmusControllerTestSet, self).__init__(*args, **kwargs)
-        self.playing = 'playing'
-        self.paused = 'paused'
         self.cmus_controller = CmusController()
     """
     Status
     """
     def test_1_status(self):
-        status = self.cmus_controller.status()
-        self.assertTrue(status == self.paused or self.playing)
+        status = self.cmus_controller.commands.status()
+        self.assertTrue(status == "paused" or "playing")
     
     """
     Pause
     """
     def test_2_pause(self):
-        self.cmus_controller.pause()
-        status = self.cmus_controller.status()
-        self.assertEqual(status, self.paused)
+        self.cmus_controller.process("!pause")
+        status = self.cmus_controller.commands.status()
+        self.assertEqual(status, "paused")
 
     """
     Play
     """
     def test_3_play(self):
-        self.cmus_controller.play()
-        status = self.cmus_controller.status()
-        self.assertEqual(status, self.playing)
+        self.cmus_controller.process("!play")
+        status = self.cmus_controller.commands.status()
+        self.assertEqual(status, "playing")
 
     """
     Raw command
     """
     def test_4_raw_command(self):
-        self.cmus_controller.raw_command('player-play')
-        status = self.cmus_controller.status()
-        self.assertEqual(status, self.playing)
+        self.cmus_controller.commands.raw_input('player-play')
+        status = self.cmus_controller.commands.status()
+        self.assertEqual(status, "playing")
 
 if __name__=="__main__":
     unittest.main(verbosity=2)
